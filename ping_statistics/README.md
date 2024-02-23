31c329587f101fed9506e11b639ee41a56852180
Jaycee <jaycee@Jaycees-Mac.lan>
2024-02-23 13:08:57 -0800

    Update ping_statistics.py with new features

--- ping_statistics/README.md
+++ ping_statistics/README.md
@@ -1,19 +1,35 @@
 ## Introduction
 
 A tool to analyze ping output:  
-1. ping pakcets:
-    a. how many transmitted
-    b. how many received
-    c. how many lost
-    d. % packet loss
-2. round-trip time:
-    a. min
-    b. avg
-    c. max
-    d. stddev
-3. percentiles:
-    default: 25th, 50th, 75th
-4. Count # of packets for consecutive packet timeouts    
+1. Pakcets Counts & Round-Trip Time:
+    a. # of packets transmitted 
+    b. # of packets received
+    c. # of packets lost
+    d. % of packets loss
+    e. RTT min
+    f. RTT avg
+    g. RTT max
+    h. RTT stddev (standard deviation)
+2. Percentile Calculation 
+    a. Constraints: 
+        Input one or more integers between 0-100
+3. Consecutive Timeouts
+    a. Notice:
+        a1. Exclude the timed-out packets that occur before the 1st successful ping packet. 
+            (As in test02.txt, ignore timeouts before line 7.)
+        a2. Exclude the timed-out packets that occur after the last successful ping packet. 
+            (As in test02.txt, ignore timeouts before line 46.)
+    b. Constraints: 
+        Input one integer
+4. Lost-Packets during a Time-Period (How many packets were lost during the requested time period?)
+    a. Constraints: 
+        a1. Input one or more integer values
+        a2. The minimum value for the duration of time should be at least 1.
+        a3. The maximum value for the duration of time would be the max packets of the input file.
+5. Output to output.csv file by default
+    a. For Lost-Packet during a Time-Period section, it would only include:
+        a1. The number of incidents that happened 
+        a2. The max number for the lost packets
 
 ## Instruction
 
@@ -26,17 +42,153 @@ A tool to analyze ping output:
 
 2. Run As:
 
+a. Help:
+
 ```console
-% python3 ping_statistics.py
+% python3 ping_statistics.py -h                                                  
+usage: ping_statistics.py [-h] [-p PERCENTILES] [-t TIMEOUT] [-c COUNT] [-o OUTPUT] filenames
 
-1. Enter a 'ping' filename: test01.txt
+Ping Statistics
 
-73 packets transmitted, 33 packets received, 40 packets lost, 121.21% packet loss
-round-trip min/avg/max/stddev = 68.441 / 245.693 / 1490.029 / 337.364 ms
-percentile 25th/50th/75th = 80.824 / 104.853 / 167.365 ms
+positional arguments:
+  filenames             List the files that contain(s) ping outputs, sepearted by ','
 
-2. Enter a number to indicate max consecutive packet timeouts ('0' to quit): 2
-seq 12548 to 12551 -     2 packets lost
-seq 12554 to 12562 -     7 packets lost
-seq 12565 to 12596 -    30 packets lost
+optional arguments:
+  -h, --help            show this help message and exit
+  -p PERCENTILES, --percentiles PERCENTILES
+                        List percentile value(s) (0-100), sepearted by ','
+  -t TIMEOUT, --timeout TIMEOUT
+                        Enter an Integer for the MAX Consecutive Timeout Packet(s)/Second(s)
+  -c COUNT, --count COUNT
+                        Enter a list of number(s) for the duration of time period, sepearted by ','
+  -o OUTPUT, --output OUTPUT
+                        Enter the output filename (.csv)
 ```
+
+b. Example 1:
+
+```console
+% python3 ping_statistics.py test02.txt,test01.txt -t 10 -p 20,50 -c 5,20
+------------------
+ test02.txt 
+------------------
+
+ // Packet Counts //
+ Transmitted       40 packets
+ Received          30 packets
+ Lost              10 packets
+ Packet Loss Rate is 25.00%
+
+ // Round-Trip Time //
+ Round-trip min    =    68.441 ms
+ Round-trip max    =  1490.029 ms
+ Round-trip avg    =   262.531 ms
+ Round-trip stddev =   349.800 ms
+
+ // Percentiles //
+ 20th Percentile is    81.271 ms
+ 50th Percentile is   116.647 ms
+
+
+ // Consecutive Timeouts //
+[ No Match on Timeout Value: '10' ]
+
+ // Lost-Packet Counts during a Time-Period //
+Per 5 packets/seconds -- 
+ Seq 12536 to 12540 lost 1 packets
+ Seq 12546 to 12550 lost 2 packets
+ Seq 12551 to 12555 lost 1 packets
+ Seq 12556 to 12560 lost 5 packets
+ Seq 12561 to 12565 lost 1 packets
+
+Per 20 packets/seconds -- 
+ Seq 12526 to 12545 lost 1 packets
+ Seq 12546 to 12565 lost 9 packets
+
+------------------
+ test01.txt 
+------------------
+
+ // Packet Counts //
+ Transmitted      219 packets
+ Received          55 packets
+ Lost             164 packets
+ Packet Loss Rate is 74.89%
+
+ // Round-Trip Time //
+ Round-trip min    =    64.031 ms
+ Round-trip max    =  1490.029 ms
+ Round-trip avg    =   232.519 ms
+ Round-trip stddev =   318.887 ms
+
+ // Percentiles //
+ 20th Percentile is    76.879 ms
+ 50th Percentile is    89.907 ms
+
+
+ // Consecutive Timeouts //
+ Seq 12565 to 12596 :    30 packets lost
+ Seq 12619 to 12712 :    92 packets lost
+ Seq 12715 to 12739 :    23 packets lost
+
+
+ // Lost-Packet Counts during a Time-Period //
+Per 5 packets/seconds -- 
+ Seq 12536 to 12540 lost 1 packets
+ Seq 12546 to 12550 lost 2 packets
+ Seq 12551 to 12555 lost 1 packets
+ Seq 12556 to 12560 lost 5 packets
+ Seq 12561 to 12565 lost 1 packets
+ Seq 12566 to 12570 lost 5 packets
+ Seq 12571 to 12575 lost 5 packets
+ Seq 12576 to 12580 lost 5 packets
+ Seq 12581 to 12585 lost 5 packets
+ Seq 12586 to 12590 lost 5 packets
+ Seq 12591 to 12595 lost 5 packets
+ Seq 12596 to 12600 lost 2 packets
+ Seq 12601 to 12605 lost 5 packets
+ Seq 12606 to 12610 lost 2 packets
+ Seq 12616 to 12620 lost 1 packets
+ Seq 12621 to 12625 lost 5 packets
+ Seq 12626 to 12630 lost 5 packets
+ Seq 12631 to 12635 lost 5 packets
+ Seq 12636 to 12640 lost 5 packets
+ Seq 12641 to 12645 lost 5 packets
+ Seq 12646 to 12650 lost 5 packets
+ Seq 12651 to 12655 lost 5 packets
+ Seq 12656 to 12660 lost 5 packets
+ Seq 12661 to 12665 lost 5 packets
+ Seq 12666 to 12670 lost 5 packets
+ Seq 12671 to 12675 lost 5 packets
+ Seq 12676 to 12680 lost 5 packets
+ Seq 12681 to 12685 lost 5 packets
+ Seq 12686 to 12690 lost 5 packets
+ Seq 12691 to 12695 lost 5 packets
+ Seq 12696 to 12700 lost 5 packets
+ Seq 12701 to 12705 lost 5 packets
+ Seq 12706 to 12710 lost 5 packets
+ Seq 12711 to 12715 lost 1 packets
+ Seq 12716 to 12720 lost 5 packets
+ Seq 12721 to 12725 lost 5 packets
+ Seq 12726 to 12730 lost 5 packets
+ Seq 12731 to 12735 lost 5 packets
+ Seq 12736 to 12740 lost 3 packets
+
+Per 20 packets/seconds -- 
+ Seq 12526 to 12545 lost 1 packets
+ Seq 12546 to 12565 lost 9 packets
+ Seq 12566 to 12585 lost 20 packets
+ Seq 12586 to 12605 lost 17 packets
+ Seq 12606 to 12625 lost 8 packets
+ Seq 12626 to 12645 lost 20 packets
+ Seq 12646 to 12665 lost 20 packets
+ Seq 12666 to 12685 lost 20 packets
+ Seq 12686 to 12705 lost 20 packets
+ Seq 12706 to 12725 lost 16 packets
+ Seq 12726 to 12744 lost 13 packets
+```
+
+3. output.csv from above example:
+[Example 1](example1.jpg)
+
+
--- ping_statistics/ping_statistics.py
+++ ping_statistics/ping_statistics.py
@@ -1,41 +1,87 @@
 '''
-1. count ping pakcets:
-    a. how many transmitted
-    b. how many received
-    c. how many lost
-    d. % packet loss
-2. round-trip:
-    a. min
-    b. avg
-    c. max
-    d. stddev
-3. percentiles:
-    default: 25th, 50th, 75th
-4. Count # of packets for consecutive packet timeouts    
+1. Pakcets Count & RTT:
+    a. # of packets transmitted 
+    b. # of packets received
+    c. # of packets lost
+    d. % of packets loss
+    e. min RTT
+    f. avg RTT
+    g. max RTT
+    h. stddev RTT
+2. Percentile Calculation
+    a. Constraints: 
+        Input one or more integer between 0-100
+3. Consecutive Timeouts
+    a. Notice:
+        a1. Exclude the timed-out packets that occur before the 1st successful ping packet. 
+            (As in test02.txt, ignore timeouts before line 7.)
+        a2. Exclude the timed-out packets that occur after the last successful ping packet. 
+            (As in test02.txt, ignore timeouts before line 46.)
+    b. Constraints: 
+        Input one integer
+4. Lost-Packets during a Time-Period (How many packets were lost during the requested time period?)
+    a. Constraints: 
+        a1. Input one or more integer values
+        a2. The minimum value for the duration of time should be at least 1.
+        a3. The maximum value for the duration of time would be the max packets of the input file.
+5. Output to output.csv file by default
+    a. For Lost-Packet during a Time-Period section, it would only include:
+        a1. The number of incidents that happened 
+        a2. The max number for the lost packets
 '''
-
 import re
 import numpy
+import argparse
+import textwrap
 import statistics
 
-def read_ping_frm_file(filename):
+
+def go_parser():
+    parser = argparse.ArgumentParser(description = 'Ping Statistics')
+    parser.add_argument('filenames',
+                        help = 'List the files that contain(s) ping outputs, sepearted by \',\'')
+    parser.add_argument('-p', '--percentiles', action = 'store', dest = 'percentiles',
+                        help = 'List percentile value(s) (0-100), sepearted by \',\'')
+    parser.add_argument('-t', '--timeout', action = 'store', dest = 'timeout',
+                        help = 'Enter an Integer for the MAX Consecutive Timeout Packet(s)/Second(s)')
+    parser.add_argument('-c', '--count', action = 'store', dest = 'count',
+                        help = 'Enter a list of number(s) for the duration of time period, sepearted by \',\'')
+    parser.add_argument('-o', '--output', action = 'store', dest = 'output',
+                        help = 'Enter the output filename (default is output.csv)')
+    args = parser.parse_args()
+    return args    
+
+
+def read_ping_frm_file(filename: str) -> dict:
     pattern = r'icmp_seq=([0-9]+).*time=([0-9]+\.[0-9]+)'
     icmp = {}
     
-    with open(filename) as f:
-        for line in f.readlines():
-            match = re.search(pattern, line.rstrip())
-            if match:
-                seq, t = match.groups()
-                icmp[int(seq)] = float(t)
+    try:
+        with open(filename) as f:
+            for line in f.readlines():
+                match = re.search(pattern, line.rstrip())
+                if match:
+                    seq, t = match.groups()
+                    icmp[int(seq)] = float(t)
+    except FileNotFoundError as e:
+        print(e)
     return icmp    
 
 
-def getPercentage(num1, num2):
+def write_to_file(lines:str, filename: str):   
+    try:
+        with open(filename, 'w') as f:
+            f.write(lines)
+    except FileNotFoundError as e:
+        print(e)
+    return True   
+
+
+def getPercentage(num1: int, num2: int) -> float:
     return (float(num1) / float(num2) * 100)
 
 
-def countMissingPackets(seq_list):
+def countMissingPackets(seq_list: list) -> int:
     count = 0
     for i in range(min(seq_list), max(seq_list) + 1):
         if i not in seq_list:
@@ -43,19 +89,7 @@ def countMissingPackets(seq_list):
     return count
 
 
-def missingPackets(seq_list):
-    missingPackets = []
-    for i in range(min(seq_list), max(seq_list) + 1):
-        if i not in seq_list:    
-            missingPackets.append(i)
-    return missingPackets
-
-
-def getTimeout(timeout_packets, seq_list):
-    #print(seq_list)
-    if not timeout_packets.isnumeric():
-        print('Invalid Input!')
-        exit()
+def getTimeout(timeout_packets:int, seq_list: list) -> list:
     if int(timeout_packets) < 1:
         exit()
     timeout_list = []
@@ -67,40 +101,143 @@ def getTimeout(timeout_packets, seq_list):
     return timeout_list
 
 
-def getPercentiles(seq_list, nums):
-    if not seq_list or not nums or type(nums) != list:
+def getPercentiles(seq_list: list, nums: list) -> dict:
+    if not seq_list or not nums:
         print('Invalid Percentile!')
         exit()
     values = {}
     for num in nums:
-        values[num] = numpy.percentile(seq_list, num)
+        try:
+            values[num] = numpy.percentile(seq_list, num)
+        except ValueError as e:
+            print(e)
     return values
-        
+
+
+def getPacketsPerTimePeriod(time_period: list, seq_list:list) -> dict:
+    if not time_period or not seq_list:
+        print('Invalid Time Period Value!')
+    periods = {}
+    for time in time_period:
+        if type(time) != int or int(time) < 1 or (time >= max(seq_list) - min(seq_list)):
+            print('Invalid Integer Value!')
+            exit()
+            
+        for i in range(0, max(seq_list) - min(seq_list) + 1, time):
+            # time period should not be over the last icmp_seq #
+            if min(seq_list) + i + time - 1 > max(seq_list):
+                last_seq = max(seq_list)
+            else:
+                last_seq = min(seq_list) + i + time - 1
+            
+            # packet count
+            packet_count = 0
+            for j in range(time):
+                if min(seq_list) + i + j > max(seq_list):
+                    break
+                if min(seq_list) + i + j not in seq_list:
+                    packet_count += 1
+
+            if time not in periods:
+                periods[time] = [[min(seq_list) + i, last_seq, packet_count]]
+            else:
+                periods[time].append([min(seq_list) + i, last_seq, packet_count])
+    return periods
+
+
 
 def main():
-    filename = input('\n1. Enter a \'ping\' filename: ')
-    icmp = read_ping_frm_file(filename)
+    args = go_parser()
+    output_filename = 'output.csv'
+    lines = ''
+    title = ''
 
-    transmitted_packets = max(icmp.keys()) - min(icmp.keys()) + 1
-    loss_packets = countMissingPackets(icmp.keys())
-    received_packets = transmitted_packets - loss_packets
-    print('\n{} packets transmitted, {} packets received, {} packets lost, {:.2f}% packet loss'.format(transmitted_packets, received_packets, loss_packets, getPercentage(loss_packets, received_packets)))
-    print('round-trip min/avg/max/stddev = {:.3f} / {:.3f} / {:.3f} / {:.3f} ms'.format(min(icmp.values()), sum(icmp.values()) / received_packets, max(icmp.values()), statistics.stdev(icmp.values())))
-    
-    request_percentiles_values = [25, 50, 75]
-    percentiles = getPercentiles(list(icmp.values()), request_percentiles_values)    
-    print('percentile 25th/50th/75th = {:.3f} / {:.3f} / {:.3f} ms'.format(percentiles[25], percentiles[50], percentiles[75]))
+    for filename in args.filenames.split(','):
+        print('------------------\n {} \n------------------'.format(filename))        
+        icmp = read_ping_frm_file(filename)
+        title = 'File Name,Transmitted,Received,Packet Loss, Loss Rate,RTT Min,RTT Max,RTT Avg,RTT STDDEV,'
+        lines += filename + ','
 
-    max_timeout_packets = input('\n2. Enter a number to indicate max consecutive packet timeouts (\'0\' to quit): ')
-    timeout_list = getTimeout(max_timeout_packets, sorted(icmp.keys()))
-    if not timeout_list:
-        print('No match!')
-    else:
-        for item in timeout_list:
-            p1, p2 = item
-            print('seq {} to {} - {:5d} packets lost'.format(p1, p2, p2 - p1 - 1))
-    print()
+        # Packet Count
+        transmitted_packets = max(icmp.keys()) - min(icmp.keys()) + 1
+        loss_packets = countMissingPackets(icmp.keys())
+        received_packets = transmitted_packets - loss_packets
+        packet_loss_rate = getPercentage(loss_packets, transmitted_packets)
+        print('\n // Packet Counts //')
+        print(' Transmitted   {:6d} packets'.format(transmitted_packets))
+        print(' Received      {:6d} packets'.format(received_packets))
+        print(' Lost          {:6d} packets'.format(loss_packets))
+        print(' Packet Loss Rate is {:5.2f}%'.format(packet_loss_rate))
+        lines += str(transmitted_packets) + ' packets,' + str(received_packets) + ' packets,' \
+                + str(loss_packets) + ' packets,' + '{:.2f}%'.format(packet_loss_rate) + '%,'
 
+        # RTT
+        print('\n // Round-Trip Time //')
+        print(' Round-trip min    = {:9.3f} ms'.format(min(icmp.values())))
+        print(' Round-trip max    = {:9.3f} ms'.format(max(icmp.values())))
+        print(' Round-trip avg    = {:9.3f} ms'.format(sum(icmp.values()) / received_packets))
+        print(' Round-trip stddev = {:9.3f} ms'.format(statistics.stdev(icmp.values())))
+        lines += '{:.3f} ms,'.format(min(icmp.values())) + '{:.3f} ms,'.format(max(icmp.values())) \
+                + '{:.3f} ms,'.format(sum(icmp.values()) / received_packets) + '{:.3f} ms,'.format(statistics.stdev(icmp.values()))
+               
+        # Percentile Calculation
+        if args.percentiles:
+            print('\n // Percentiles //')
+            try:
+                percentiles_values = list(map(int, re.split(r'\D+', args.percentiles)))
+                percentiles = getPercentiles(list(icmp.values()), percentiles_values)
+                if not percentiles:
+                    print('[ Error occured on calculating percentile! ]')
+                else:
+                    print(''.join(['{:3d}th Percentile is {:9.3f} ms\n'.format(percentile, value) for percentile, value in percentiles.items()]))
+                    title += ''.join(['{:3d}th Percentile,'.format(p) for p in percentiles.keys()])
+                    lines +=  ''.join(['{:.3f} ms,'.format(percentiles[p]) for p in percentiles.keys()])                    
+            except ValueError as e:
+                print('[ Error on Percentiles: {} ]'.format(e))
+
+        # Consecutive Timeouts
+        if args.timeout:
+            print('\n // Consecutive Timeouts //')
+            if not args.timeout.isnumeric() or int(args.timeout) < 0 or int(args.timeout) >= max(icmp.keys()) -  min(icmp.keys()):
+                print('[ Error on Timeout Value: \'{}\' ]'.format(args.timeout))
+            else:
+                timeout_list = getTimeout(args.timeout, sorted(icmp.keys()))
+                title += 'Consecutive Timeout ' + str(args.timeout) + ' packets,'
+                if not timeout_list:
+                    print('[ No Match on Timeout Value: \'{}\' ]'.format(args.timeout))
+                    lines += 'No Match!,'
+                else:
+                    print(''.join([' Seq {} to {} : {:5d} packets lost\n'.format(p1, p2, p2 - p1 - 1) for p1, p2 in timeout_list]))
+                    lines += '"'
+                    lines += ''.join(['Seq {} to {} : {:d} packets lost\n'.format(p1, p2, p2 - p1 - 1) for p1, p2 in timeout_list])
+                    lines += '",'
+
+        # Lost-Packet Count during a Time-Period
+        if args.count:
+            print('\n // Lost-Packet Counts during a Time-Period //')
+            try:
+                time_period_values = list(map(int, re.split(r'\D+', args.count)))
+                time_periods = getPacketsPerTimePeriod(time_period_values, sorted(icmp.keys()))
+                for time_period, v in time_periods.items():
+                    print('Per {} packets/seconds -- '.format(time_period))
+                    print(''.join([' Seq {} to {} lost {} packets\n'.format(p1, p2, count) for p1, p2, count in v if count > 0]))
+                    title += '# of Incidents Per '+ str(time_period) + ' Packets,'
+                    sum_incident = max_packet_lost = 0
+                    for *p,count in v:
+                        if count > 0:
+                            sum_incident += 1
+                        if count > max_packet_lost:
+                            max_packet_lost = count
+                    lines += '{} Incidents (Max {} packets lost),'.format(sum_incident, max_packet_lost)
+            except ValueError as e:
+                print('[ Error on Lost-Packet Count: {} ]'.format(e))        
+        lines += '\n'
+
+    # Output to a CSV file
+    if args.output:
+        output_filename = args.output            
+    #print(title + '\n' + lines)      
+    write_to_file(title + '\n' + lines, output_filename)
 
 if __name__ == '__main__':
     main()
